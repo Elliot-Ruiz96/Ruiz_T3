2b4ad53803f8439a940219f83e66b6dcde750a19
Elliot Ruiz Sánchez <elliotruizs@ieee.org>
2021-06-08 10:52:47 -0500

    Update main.c

--- main.c
+++ main.c
@@ -6,14 +6,6 @@
 
 #define PIN4      	4u
 #define	PIN6		6u
-#define PIN21      	21u
-#define	PIN22		22u
-#define PIN26		26u
-
-gpio_pin_config_t led_config = {
-        kGPIO_DigitalOutput,
-        1,
-    };
 
 gpio_pin_config_t sw_config = {
        kGPIO_DigitalInput,
@@ -35,58 +27,22 @@ int main(void) {
 	// Habilitamos los relojes de los Puertos a usar
 
 	CLOCK_EnableClock(kCLOCK_PortA);								// Switch 3
-	CLOCK_EnableClock(kCLOCK_PortB);								// Red and Blue LED
 	CLOCK_EnableClock(kCLOCK_PortC);								// Switch 2
-	CLOCK_EnableClock(kCLOCK_PortE);								// Green LED
 
 	// Funcion de GPIO
 
-	PORT_SetPinMux(PORTB, PIN21, kPORT_MuxAsGpio); 					// Blue LED GPIO
-	PORT_SetPinMux(PORTB, PIN22, kPORT_MuxAsGpio); 					// Red LED GPIO
-	PORT_SetPinMux(PORTE, PIN26, kPORT_MuxAsGpio); 					// Green LED GPIO
 	PORT_SetPinMux(PORTA, PIN4, kPORT_MuxAsGpio); 					// Switch 3 GPIO
 	PORT_SetPinMux(PORTC, PIN6, kPORT_MuxAsGpio); 					// Switch 2 GPIO
 
-	PORT_SetPinConfig(PORTA, PIN4, &sw_pin_config);				// Activacion de Switch 3
-	PORT_SetPinConfig(PORTC, PIN6, &sw_pin_config);				// Activacion de Switch 2
+	PORT_SetPinConfig(PORTA, PIN4, &sw_pin_config);					// Activacion de Switch 3
+	PORT_SetPinConfig(PORTC, PIN6, &sw_pin_config);					// Activacion de Switch 2
 
-	GPIO_PinInit(GPIOB, PIN22, &led_config);
-	GPIO_PinInit(GPIOB, PIN21, &led_config);
-	GPIO_PinInit(GPIOE, PIN26, &led_config);
-	GPIO_PinInit(GPIOA, PIN4, &swe_config);
+	GPIO_PinInit(GPIOA, PIN4, &sw_config);
 	GPIO_PinInit(GPIOC, PIN6, &sw_config);
 
     while(1){
 
-    	// Start
 
-    	GPIO_PortToggle(GPIOB, 1u << PIN22);					// YELLOW
-    	GPIO_PortToggle(GPIOE, 1u << PIN26);
-    	GPIO_PortToggle(GPIOB, 1u << PIN22);					// RED
-    	GPIO_PortToggle(GPIOB, 1u << PIN21);					// PURPLE
-    	GPIO_PortToggle(GPIOB, 1u << PIN22);
-
-    	// Sw2
-
-    	GPIO_PortToggle(GPIOE, 1u << PIN26);					// GREEN
-    	GPIO_PortToggle(GPIOB, 1u << PIN21);					// BLUE
-    	GPIO_PortToggle(GPIOB, 1u << PIN22);					// RED
-
-    	// Sw3
-
-    	GPIO_PortToggle(GPIOB, 1u << PIN21);					// BLUE
-    	GPIO_PortToggle(GPIOB, 1u << PIN21);					// WHITE
-    	GPIO_PortToggle(GPIOB, 1u << PIN22);
-    	GPIO_PortToggle(GPIOE, 1u << PIN26);
-    	GPIO_PortToggle(GPIOB, 1u << PIN22);					// RED
-
-    	// Sw3 x2
-
-    	GPIO_PortToggle(GPIOE, 1u << PIN26);					// GREEN
-    	GPIO_PortToggle(GPIOB, 1u << PIN21);					// PURPLE
-    	GPIO_PortToggle(GPIOB, 1u << PIN22);
-    	GPIO_PortToggle(GPIOB, 1u << PIN22);					// YELLOW
-    	GPIO_PortToggle(GPIOE, 1u << PIN26);
 
     }
     return 0 ;
