91c9835f3ed8ea115119c74c2f74440de34249c4
Elliot Ruiz Sánchez <elliotruizs@ieee.org>
2021-06-09 00:18:50 -0500

    Create RGB.h

---
+++ RGB.h
@@ -0,0 +1,20 @@
+#ifndef RGB_H_
+#define RGB_H_
+
+#include "fsl_gpio.h"
+
+#define CORE_FREQ	21000000u
+#define DELAY		1000000u
+
+#define PIN22       22u
+#define PIN21       21u
+#define PIN26		26u
+
+void RED_RGB();
+void PURPLE_RGB();
+void BLUE_RGB();
+void WHITE_RGB();
+void YELLOW_RGB();
+void GREEN_RGB();
+
+#endif /* RGB_H_ */
